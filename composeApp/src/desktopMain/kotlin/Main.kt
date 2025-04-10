9396244cf1bfd6259dd5ca546b35b48b21dce773
Julian Egner <j.egner@tarent.de>
2025-04-10 10:32:38 +0200

    start game in maximized window for desktop

--- composeApp/src/desktopMain/kotlin/Main.kt
+++ composeApp/src/desktopMain/kotlin/Main.kt
@@ -1,9 +1,16 @@
 // import App
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "CoShaNu") {
+    val state = rememberWindowState(placement = WindowPlacement.Maximized)
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "CoShaNu",
+        state = state
+    ) {
         App()
     }
 }
