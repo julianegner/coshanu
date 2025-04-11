8b1945739f25de086d17bc087e8e9b0fe7aea9f8
Julian Egner <j.egner@tarent.de>
2025-04-11 11:20:48 +0200

    renaming compile results, adding apllicatin icon for desktop

--- composeApp/src/desktopMain/kotlin/Main.kt
+++ composeApp/src/desktopMain/kotlin/Main.kt
@@ -3,13 +3,17 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import coshanu.composeapp.generated.resources.Res
+import coshanu.composeapp.generated.resources.icon
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
     val state = rememberWindowState(placement = WindowPlacement.Maximized)
     Window(
         onCloseRequest = ::exitApplication,
         title = "CoShaNu",
-        state = state
+        state = state,
+        icon = painterResource(Res.drawable.icon)
     ) {
         App()
     }
