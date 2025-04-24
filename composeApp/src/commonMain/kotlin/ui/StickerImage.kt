3af363e1ed938ccb30350bc0ef60dc81f751ba2d
Julian Egner <j.egner@tarent.de>
2025-04-24 13:40:42 +0200

    adjust sticker image

--- composeApp/src/commonMain/kotlin/ui/StickerImage.kt
+++ composeApp/src/commonMain/kotlin/ui/StickerImage.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
@@ -18,6 +20,24 @@ creating an image for a sticker
 @Composable
 fun StickerImage() {
     UiStateHolder.darkModeState.value = false
+
+    /*
+    Row (modifier = Modifier
+        .fillMaxWidth()
+        // .height(50.dp)
+        .background(Color.Black)
+
+    ){
+        Text(
+            text = "play",
+            color = Color.White,
+            fontStyle = FontStyle.Italic,
+            fontSize = TextUnit(2f, TextUnitType.Em),
+            modifier = Modifier.padding(start = 200.dp, top = 100.dp)
+        )
+    }
+     */
+
     Row (modifier = Modifier
         .fillMaxWidth()
         .height(250.dp)
@@ -35,6 +55,24 @@ fun StickerImage() {
             ),
             modifier = Modifier.width(200.dp).height(100.dp)
         )
+
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "play",
+                color = Color.White,
+                fontStyle = FontStyle.Italic,
+                fontSize = TextUnit(3f, TextUnitType.Em),
+                modifier = Modifier.padding(top = 40.dp)
+            )
+            Text(
+                text = "www.",
+                color = Color.White,
+                fontSize = TextUnit(2f, TextUnitType.Em),
+                modifier = Modifier.padding(top = 50.dp)
+            )
+        }
         Text(
             text = "cosha.nu",
             color = Color.White,
