f90742396fd9cabc1309467c4b5cdaeee8c5ebbe
Julian Egner <j.egner@tarent.de>
2025-04-25 20:46:22 +0200

    add app loading info to wasm

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,7 +1,9 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
+import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    document.getElementById("loader-container")?.remove()
     CanvasBasedWindow(canvasElementId = "ComposeTarget") { App() }
 }
