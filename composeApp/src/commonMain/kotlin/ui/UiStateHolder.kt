632423e955783ae0166bcd099ac36bf95c40a82b
Julian Egner <j.egner@tarent.de>
2025-05-19 16:55:04 +0200

    replace audiobyte with soundboard

--- composeApp/src/commonMain/kotlin/ui/UiStateHolder.kt
+++ composeApp/src/commonMain/kotlin/ui/UiStateHolder.kt
@@ -27,7 +27,7 @@ object UiStateHolder {
     val displaySettingsArea: MutableState<Boolean> = mutableStateOf(false)
     val displayStickerArea: MutableState<Boolean> = mutableStateOf(false)
     val soundActive: MutableState<Boolean> = mutableStateOf(true)
-    val sound = SoundBoard("")
+    val sound = SoundBoardTT("")
 
     fun setScreenType(newScreenType: ScreenType) {
         screenType.value = newScreenType
