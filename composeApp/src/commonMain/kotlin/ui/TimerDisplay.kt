f7d670c6e94c021d46924153b0a99629617fbc4c
Julian Egner <j.egner@tarent.de>
2025-07-15 17:08:14 +0200

    remove unused import

--- composeApp/src/commonMain/kotlin/ui/TimerDisplay.kt
+++ composeApp/src/commonMain/kotlin/ui/TimerDisplay.kt
@@ -9,8 +9,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import coshanu.composeapp.generated.resources.Res
-import coshanu.composeapp.generated.resources.time_is_up
 import game.GameStateHolder
 import game.GameStateHolder.timer
 import game.GameStateHolder.updateGameState
