63c62241117955368edc6c3f7b61aaf1b068d13f
Julian Egner <j.egner@tarent.de>
2025-10-10 11:12:44 +0200

    add spacing to the top for mobile

--- composeApp/src/commonMain/kotlin/Platform.kt
+++ composeApp/src/commonMain/kotlin/Platform.kt
@@ -10,6 +10,7 @@ expect fun getPlatform(): Platform
 val isPlatformWasm = ("Web with Kotlin/Wasm" == getPlatform().name)
 val isPlatformAndroid = getPlatform().name.startsWith("Android")
 val isPlatformIos = getPlatform().name.startsWith("iOS")
+val isPlatformMobile = isPlatformAndroid || isPlatformIos
 
 val isLandscape: Boolean = UiStateHolder.screenType.value == ScreenType.LANDSCAPE
 val isPortrait: Boolean = UiStateHolder.screenType.value == ScreenType.PORTRAIT
